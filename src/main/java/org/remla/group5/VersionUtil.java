d1e379c8b794681f3b1b86fb858ef5dc8a640b1b
Ivan Todorov <ivan.s.t@icloud.com>
2023-05-08 19:41:14 +0200

    Updated the VersionUtil class

--- src/main/java/org/remla/group5/VersionUtil.java
+++ src/main/java/org/remla/group5/VersionUtil.java
@@ -1,8 +1,12 @@
 package org.remla.group5;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class VersionUtil {
-    public static String getVersion() {
-//      TODO: improve this code
-        return "0.0.1";
+    public static String getVersion() throws IOException {
+        final Properties properties = new Properties();
+        properties.load(VersionUtil.class.getClassLoader().getResourceAsStream("application.properties"));
+        return properties.getProperty("version");
     }
 }
